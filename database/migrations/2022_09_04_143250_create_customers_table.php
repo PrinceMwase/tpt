b35418f5d2e208c02f68904aa3b219e4b6ca5a70
Prince Mwase <princefranklinemwase@gmail.com>
2022-09-05 01:05:06 +0200

    created initial migrations, policies, controllers, seeders and models

---
+++ database/migrations/2022_09_04_143250_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('primary_phone_number')->unique();
+            $table->enum('whatsapp_status', [
+                'online',
+                'offline'
+            ])->default("offline");
+            $table->string('secondary_phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2022_09_04_143922_create_transactions_table.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            
+
+            // transaction status
+            $table->enum('status', [
+                "delivered",
+                "In Transit",
+                "verified"
+            ] );
+
+            // sender fields to fill
+            $table->enum('service_type', [
+                "door to door",
+                "office collection",
+                "rush",
+                "city delivery",
+                "city pickup"
+
+            ]);
+
+            $table->integer("customer_id");
+
+            $table->string("receiver_name");
+
+            $table->string("receiver_phone");
+
+            $table->enum("whatsapp_status", [
+                "online",
+                "offline"
+            ])->default("offline");
+
+            $table->enum("fragile", [
+                "yes",
+                "no"
+            ])->default("no");
+
+            $table->enum("electronics", [
+                "yes",
+                "no"
+            ])->default("no");
+
+            $table->text("location");
+
+            $table->string("district");
+
+            // office us
+            $table->string("waybill_number")->nullable();
+            $table->integer("amount")->nullable();
+            $table->string("payment_term");
+
+
+            $table->timestamps();
+
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
