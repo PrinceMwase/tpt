d268b0214cfca6e9442536febf3556eb97dd393f
Prince Mwase <princefranklinemwase@gmail.com>
2022-09-06 21:08:36 +0200

    registrarion and login that returns access token

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
+use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +21,64 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * Login
+ */
+
+
+/**
+ * get Token by logging in
+ */
+Route::post('/sanctum/token', function (Request $request) {
+     $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $credentials = Array("email" => $request->email, "password" => $request->password);
+
+    $user = User::where('email', $request->email)->first();
+    
+    if ($user ) {
+        Auth::attempt($credentials);
+    }
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+// register new user
+Route::post("/sanctum/register", function(Request $request) {
+    $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        'device_name' => 'required',
+    ]);
+
+    $credentials = Array("email" => $request->email, "password" => $request->password);
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    if ($user ) {
+        Auth::attempt($credentials);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+} );
+
+// get currrent User
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
