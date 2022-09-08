b1f3e2211e17aedf62292216f32bee9e319487fa
Prince Mwase <princefranklinemwase@gmail.com>
2022-09-08 03:02:07 +0200

    seeders and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 courier service backend
+----------
+
+```requirements```
+`php` version 7.4.19 and above
+`composer` to install dependancies
+
+How to run
+
+in the terminal run:
+
+# composer install
+
+# php artisan migrate:fresh --seed
+
+# php artisan serve
+
+```Notice```
 The admin account should have id as 1 or else the app will break
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create();
+
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'agent@gmail.com',
+            'role_id' => 3,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+            ]);
+
+        DB::table('roles')->insert([
+            'role' => 'admin'
+            ]);
+        DB::table('roles')->insert([
+            'role' => 'customer'
+            ]);
+        DB::table('roles')->insert([
+            'role' => 'agent'
+            ]);
+
     }
 }
